2c5d137e6d9243dea0fae61860ac69e2e8f6213f
AmilcarArmmand <armmand@gmail.com>
2020-02-08 04:10:11 +0000

    Fixed output to print 179 characters instead of 180

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,23 +9,21 @@
 int main(void)
 {
 	int i;
-	int j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 99; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		if ((i / 10)  < (i % 10))
 		{
-			if (i < j)
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			if (i / 10 != 8)
 			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if (i != 8 || j != 9)
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
 
-/*	putchar('\n'); */
+	putchar('\n');
 	return (0);
 }
