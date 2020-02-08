f8a44121a18c8f49b54c0f9cf2ac901ca0d91c50
AmilcarArmmand <armmand@gmail.com>
2020-02-08 03:27:30 +0000

    Changed to two for loops

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /**
  * main - program that prints all possible numbers fo two digit numbers
  *
@@ -10,28 +11,21 @@ int main(void)
 {
 	int i;
 	int j;
-	int k;
-	int l;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 100; j++)
 		{
-			for (k = 0; k < 10; k++)
-			{
-				for (l = 0; l < 10; l++)
+			if (i < j)
 				{
-					if (i < j || k < l)
-					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(' ');
-						putchar(k + '0');
-						putchar(l + '0');
+					putchar(i/10 + '0');
+					putchar(i%10 + '0');
+					putchar(' ');
+					putchar(j/10 + '0');
+					putchar(j%10 + '0');
+					if (i < 98)
 						putchar(',');
-					}
 				}
-			}
 		}
 	}
 
