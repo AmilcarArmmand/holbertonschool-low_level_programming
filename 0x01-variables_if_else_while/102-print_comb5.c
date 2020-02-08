36ee15bbefb129077a5e13f1b0369fdf2f2ceaf3
AmilcarArmmand <armmand@gmail.com>
2020-02-08 03:45:45 +0000

    Changed spaces to tabs to be Betty linter compliant

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,21 +17,20 @@ int main(void)
 		for (j = 0; j < 100; j++)
 		{
 			if (i < j)
+			{
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+				if (i < 98)
 				{
-					putchar(i / 10 + '0');
-					putchar(i % 10 + '0');
+					putchar(',');
 					putchar(' ');
-					putchar(j / 10 + '0');
-					putchar(j % 10 + '0');
-					if (i < 98)
-					{
-						putchar(',');
-						putchar(' ');
-					}
 				}
+			}
 		}
 	}
-
 	putchar('\n');
 
 	return (0);
