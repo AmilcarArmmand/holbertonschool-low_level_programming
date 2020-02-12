876c3422c53290b48c722b579e83287831fbe29e
AmilcarArmmand <armmand@gmail.com>
2020-02-12 22:56:10 +0000

    Removed equal sing from line 17

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,9 +14,9 @@
 {
 	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 	{
-		if (month >= 2 && day >= 60)
+		if (month >= 2 && day > 60)
 		{
-/*			day++; */
+			day++;
 		}
 
 		printf("Day of the year: %d\n", day);
