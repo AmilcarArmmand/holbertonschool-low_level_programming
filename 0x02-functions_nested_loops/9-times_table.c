4cf8fb650170b84f5c58f884c647890316e17b80
AmilcarArmmand <armmand@gmail.com>
2020-02-12 19:54:28 +0000

    Updated task 9, fixed loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * times_table - a function that prints every minute of the day.
  *
@@ -7,36 +6,43 @@
  */
 void times_table(void)
 {
-	int num1;
-	int num2;
-	int prod;
+	int num1, num2, prod;
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
 		for (num2 = 0; num2 <= 9; num2++)
 		{
 			prod = num1 * num2;
-			if (num1 < 1 && prod < 1)
+			if (num2 == 0 && prod == 0)
 			{
 				_putchar('0' + prod);
 				_putchar(',');
 			}
-			else if (prod <= 9 && num2 < 9)
+			else if (prod < 10 && num2 < 9)
 			{
 				_putchar(' ');
 				_putchar(' ');
 				_putchar('0' + prod);
 				_putchar(',');
 			}
-			else
+			else if (prod >= 10 && num2 < 9)
 			{
 				_putchar(' ');
 				_putchar('0' + prod / 10);
 				_putchar('0' + prod % 10);
 				_putchar(',');
 			}
-		}
-		_putchar('\n');
-	}
+			else
+			{
+				_putchar(' ');
+				if (prod < 10)
+					_putchar(' ');
+				else
+					_putchar('0' + prod / 10);
 
+				_putchar('0' + prod % 10);
+			}
+		}
+			_putchar('\n');
+	}
 }
