1c4b50a309e32a385619d1e24c82a3d08274f4f6
AmilcarArmmand <armmand@gmail.com>
2020-02-12 02:34:09 +0000

    Added task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Prints the sum of the natural numbers that are multiples
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	int sum;
+	int i;
+
+	for (i = 0, num = 1024; i < num; i++)
+		if (i % 3 == 0 || i % 5 == 0 )
+			sum += i;
+
+	printf("%d\n", sum);
+	return (0);
+}
