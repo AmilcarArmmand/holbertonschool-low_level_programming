4b25e5ea1c8c6aa2cc9181219bcdee9e3e5d2abb
AmilcarArmmand <armmand@gmail.com>
2020-02-13 01:32:07 +0000

    Modified to have under 40 lines of code to be betty linter compliant

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * print_times_table - a function that prints n times table, starting with 0.
- *
+ * @n: integer value from main function
  */
 void print_times_table(int n)
 {
@@ -15,9 +15,7 @@ void print_times_table(int n)
 			{
 				prod = num1 * num2;
 				if (num2 == 0)
-				{
 					_putchar('0' + prod);
-				}
 				else if (prod < 10)
 				{
 					_putchar(',');
@@ -46,6 +44,4 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
-	else
-		return;
 }
