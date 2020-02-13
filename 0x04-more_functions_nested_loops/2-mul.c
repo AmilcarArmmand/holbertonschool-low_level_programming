13dae7d3e11affa79624359df6401b674a7cc699
AmilcarArmmand <armmand@gmail.com>
2020-02-13 15:56:54 +0000

    Added task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - A function that multiplies two integers
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
