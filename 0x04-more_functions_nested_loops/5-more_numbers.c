44079c23b78e5f97e32808aeda971c4939e8d068
AmilcarArmmand <armmand@gmail.com>
2020-02-13 19:14:01 +0000

    Removed the fourth _putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,18 +8,24 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int num1;
+	int num2;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j < 10)
-				_putchar('0' + j);
+			if (j >= 10)
+			{
+				num1 = j / 10;
+				num2 = j % 10;
+				_putchar('0' + num1);
+			}
 			else
 			{
-				_putchar('0' + j / 10);
-				_putchar('0' + j % 10);
+				num2 = j;
 			}
+			_putchar('0' + num2);
 		}
 		_putchar('\n');
 	}
