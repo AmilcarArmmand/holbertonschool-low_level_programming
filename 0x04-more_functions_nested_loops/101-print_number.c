d95a2364cdd07d2078c03ce884962c96771b5396
AmilcarArmmand <armmand@gmail.com>
2020-02-14 00:52:31 +0000

    Added task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_number - A function that prints an integer
+ * @n: integer value to print
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+/*	if (n == 0)  */
+/*		_putchar('0');  */
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+
+}
