7bb3a61fc3613869e3c50a70bbb62b5c5dbf7bd6
AmilcarArmmand <armmand@gmail.com>
2020-02-18 14:24:15 +0000

    Added task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_array - a function that prints n elements of an array of integers
+ * @a: pointer to an array of integers
+ * @n: number of elements to print
+ *
+ * Return: void (No return data)
+ */
+void print_array(int *a, int n)
+{
+	int counter;
+
+	for (counter = 0; counter < n; counter++)
+	{
+		printf("%d", a[counter]);
+		{
+			if (counter != (n-1))
+			    printf(", ");
+		}
+	}
+	printf("\n");
+}
