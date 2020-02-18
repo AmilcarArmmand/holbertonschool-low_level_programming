29f7e556ad6c93d8bb3e8948c397982593f271c4
AmilcarArmmand <armmand@gmail.com>
2020-02-18 14:19:21 +0000

    Added task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - reset pointer to int and updates the value it points to to 98.
+ * @n: pointer to an int
+ *
+ * Return: void (No value returned)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
