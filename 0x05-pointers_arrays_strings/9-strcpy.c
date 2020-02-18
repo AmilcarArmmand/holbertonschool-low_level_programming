bbb76ea44d5f58185c3fb8f28058bcfbbc033c88
AmilcarArmmand <armmand@gmail.com>
2020-02-18 21:01:20 +0000

    Added task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - a function that copies a string including the '\0'
+ * @dest: copy of string pointed to by src
+ * @src: string to be copied
+ *
+ * Return: *_strcpy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;  /* counter for elements copied */
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
