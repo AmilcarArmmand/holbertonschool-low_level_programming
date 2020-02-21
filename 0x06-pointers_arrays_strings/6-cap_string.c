b2642e78f803b5c29412c1cda9737eab71aba875
AmilcarArmmand <armmand@gmail.com>
2020-02-21 02:45:13 +0000

    Added task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * cap_string -  a function that capitalizes all words of a string
+ * @s: string argument
+ *
+ * Return: string
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int j;
+/*	int len1; */
+
+	char separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(',
+			     ')', '{', '}'};
+
+/*	for (len1 = 0; s[len1] != '\0'; len1++) */
+/*		; */
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (separators[j] == s[i])
+			{
+				if (s[0] == separators[j])
+					s[0] -= 32;
+
+				else if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] -= 32;
+			}
+		}
+	}
+	return (s);
+}
