4ce18f8881ab03ba986a8416fb39f92d9d84e8c1
AmilcarArmmand <armmand@gmail.com>
2020-02-24 18:28:59 +0000

    Added task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strchr - a function that locates a character string
+ * @s: string containing char c.
+ * @c: character that will be searhed for within string s.
+ *
+ * Return: pointer to first occurrence of the character c in the string.
+ */
+char *_strchr(char *s, char c)
+{
+	while (s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
