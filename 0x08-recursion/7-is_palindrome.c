de8476fdd0e472ab9a8d6c8fbcdf6dda26be3aad
AmilcarArmmand <armmand@gmail.com>
2020-02-27 02:55:14 +0000

    Added draft of task 7

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * is_palindrome - a function that returns 1 if string is palindrome, 0 if not.
+ * @s: pointer to a string
+ *
+ * Return: 1 (Success) or 0 (if not)
+ */
+int is_palindrome(char *s)
+{
+	int i;
+
+	i = 0;
+
+	if (*s == '\0')
+		return (0);
+
+	if (*s != '\0')
+	{
+/* 		i = _strlen_recursion(s + 1); */
+	}
+	return (1 + i);
+}
