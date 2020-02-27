1c0989b4e69124cc85c83b9704101b8226a473ab
AmilcarArmmand <armmand@gmail.com>
2020-02-27 02:30:39 +0000

    Added counter variable

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,16 @@
  */
 int _strlen_recursion(char *s)
 {
+	int i;
+
+	i = 0;
+
 	if (*s == '\0')
 		return (0);
 
 	if (*s != '\0')
 	{
-		_strlen_recursion(s + 1);
+		i = _strlen_recursion(s + 1);
 	}
-	return (1 + _strlen_recursion(s + 1));
+	return (1 + i);
 }
