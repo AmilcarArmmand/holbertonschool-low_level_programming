7038fa72beaaf4078d28ca928dc15f1e856a19bd
AmilcarArmmand <armmand@gmail.com>
2020-02-28 04:57:47 +0000

    Added description of parameter n

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,21 +2,21 @@
 
 /**
  * is_prime_number - a function that determines if a number is a prime number.
+ * @n: integer to test if it is a prime number.
  *
  * Return: 1 (Success) or 0 (false)
  */
 int is_prime_number(int n)
 {
-	int x;
+	int result;
 
-	x = 2;
+	result = 0;
 
-	if (n <= 2)
-		return (1);
-	if (n % 2 == 0 && x > 1)
-		return (-1);
-	else if (x == 1)
-		return (0);
-	is_prime_number(n - 1);
-	return (0);
+	if (n <= 2 && n % 2 == 0 && n != 2)
+		result = 0;
+	else
+/*		(is_prime_number(n * is_prime_number((n / 2) - 1)); */
+		result = 1;
+
+return (result);
 }
