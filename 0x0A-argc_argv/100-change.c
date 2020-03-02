bb816ab79ffb39625882e8814597c34d57759204
AmilcarArmmand <armmand@gmail.com>
2020-03-02 04:28:12 +0000

    Added draft

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - a program prints the minimum number of coins to give in change.
+ * @argc: argument count
+ * @argv: argument vector containing two numbers.
+ *
+ * Return: 0 on success, 1 on error.
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+
+	return (0);
+}
