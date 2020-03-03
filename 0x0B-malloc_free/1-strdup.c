904ecd57349a7a9bfdcf6571e93f591e58aca9f7
AmilcarArmmand <armmand@gmail.com>
2020-03-03 19:04:42 +0000

    Added task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _strdup - function that returns a pointer to a newly allocated memory space
+ * @str: a pointer to a char string
+ *
+ * Return: a pointer to the duplicated string or NULL if failed
+ */
+char *_strdup(char *str)
+{
+	unsigned int i;
+	int len;
+	char *dupstring;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	dupstring = malloc(sizeof((*str) * len));
+	if (dupstring == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		dupstring[i] = str[i];
+
+	dupstring[len] = '\0';
+	return (dupstring);
+}
