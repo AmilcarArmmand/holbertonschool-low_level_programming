4726493f6f0a2cfcb8e008ea76d9e737f4bd68fe
AmilcarArmmand <armmand@gmail.com>
2020-03-03 21:53:40 +0000

    Updated to check if NULL is passed, treat as empty string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,8 +12,8 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, len1, len2;
 	char *newstring;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+/*	if (s1 == NULL && s2 == NULL) */
+/*		return (NULL); */
 
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
