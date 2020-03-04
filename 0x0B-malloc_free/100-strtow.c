7b6ebea1d6f04049ac75eb4a9cc9f9c06886667b
AmilcarArmmand <armmand@gmail.com>
2020-03-04 03:39:27 +0000

    Working draft

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * strtow -  a function that splits a string into words.
+ * @str: string that will be split into words.
+ *
+ * Return: pointer to a word string or NULL.
+ */
+char **strtow(char *str)
+{
+	(void)str;
+	return (NULL);
+}
