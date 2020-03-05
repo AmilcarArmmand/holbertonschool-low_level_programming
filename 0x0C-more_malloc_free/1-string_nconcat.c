599b48cbc4dde42d7b6232e2d9de59f84998551c
AmilcarArmmand <armmand@gmail.com>
2020-03-05 22:20:33 +0000

    Removed + 1 from line 28

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, len1, len2;
 	char *dest;
 
-	len1 = len2 = i = 0;
+	i = len1 = len2 = 0;
 
 	if (s1 != NULL)
 	{
@@ -25,8 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		for (len2 = 0; s2[len2] != '\0'; len2++)
 			;
 	}
-	dest = malloc((len1 + len2 + 1) * sizeof(char) + 1);
-
+	dest = malloc((len1 + len2) * sizeof(char) + 1);
 	if (dest == NULL)
 	{
 		free(dest);
