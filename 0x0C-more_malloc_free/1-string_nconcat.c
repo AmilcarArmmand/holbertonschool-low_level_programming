bd4107c028b0657e73be6d662507a8a205111606
AmilcarArmmand <armmand@gmail.com>
2020-03-06 00:54:40 +0000

    Changed size of memory to allocate to len1 + n + 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	dest = malloc((len1 + len2 + 1) * sizeof(char) + 1);
+	dest = malloc((len1 + n + 1) * sizeof(char) + 1);
 	if (dest == NULL)
 	{
 		free(dest);
