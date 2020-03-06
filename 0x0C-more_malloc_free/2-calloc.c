55d94e0fed1c6432387cc74994aa7e4db8c97cb7
AmilcarArmmand <armmand@gmail.com>
2020-03-06 00:36:49 +0000

    Updated array initialization

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	buffer = malloc(nmemb * size);
 	if (buffer == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
+	for (i = 0; i < nmemb * size; i++)
 		buffer[i] = 0;
 	return (buffer);
 }
