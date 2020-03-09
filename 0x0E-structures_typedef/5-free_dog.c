889d748755b671ab74f575ca779865f46dedea7a
AmilcarArmmand <armmand@gmail.com>
2020-03-09 23:55:36 +0000

    Added drafts of tasks 4 and taks 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - a function that frees memory from a struct.
+ * @d: name component of struct.
+ *
+ * Return: void (No return data type)
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
