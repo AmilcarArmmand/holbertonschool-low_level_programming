46b08412390fc47a59dc6c9fe3dee327e29c0fea
AmilcarArmmand <armmand@gmail.com>
2020-03-10 02:11:44 +0000

    Added memory allocation for the name and owner elements of the struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,14 +20,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-
 	new_dog->name = name;
+	new_dog->name = malloc(((sizeof(name)) * (sizeof(char) + 1)));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog->name);
+		return (NULL);
+	}
 	new_dog->age = age;
 	new_dog->owner = owner;
-
-
-
-/*	new_dog = malloc((sizeof(char *(name)) + sizeof((char) *(owner))); */
-
+	new_dog->owner = malloc(((sizeof(owner)) * (sizeof(char) + 1)));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->owner);
+		return (NULL);
+	}
+	free(new_dog);
 	return ((new_dog == NULL) ? NULL : new_dog);
 }
