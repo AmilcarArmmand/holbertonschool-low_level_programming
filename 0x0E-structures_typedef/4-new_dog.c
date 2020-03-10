c5852634809cac32d79581531a45058419a18c72
AmilcarArmmand <armmand@gmail.com>
2020-03-10 00:11:51 +0000

    Moved struct elements below new_dog malloc code block

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,16 +14,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
-
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
+
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
+
+
 /*	new_dog = malloc((sizeof(char *(name)) + sizeof((char) *(owner))); */
 
 	return ((new_dog == NULL) ? NULL : new_dog);
