353402d1efa7588ceba909865da4958436b69641
AmilcarArmmand <armmand@gmail.com>
2020-03-11 16:20:51 +0000

    Added description of name parameter and added check for name is NULL

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,12 +2,14 @@
 
 /**
  * print_name - a function that prints a name.
- * @name:
+ * @name: char string from main.
  * @f: function pointer to a char string
  *
  * Return: void (No data type returned)
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL)
+		exit(0);
 	(*f)(name);
 }
