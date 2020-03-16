2ad110919a59b0e6202ada4e43291eb0ed014b7f
AmilcarArmmand <armmand@gmail.com>
2020-03-16 02:09:25 +0000

    Added struct and prototype functions for 3-print_all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,10 +3,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list arguments);
+void print_integer(va_list arguments);
+void print_float(va_list arguments);
+void print_string(va_list arguments);
+
+typedef struct datatype
+{
+	char *typedata;
+	void (*printfunction)();
+} dtype;
 
 #endif /* VARIADIC_FUNCTIONS_H */
