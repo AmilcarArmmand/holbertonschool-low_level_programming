7d20e5b6c04b6389955134016563e44b40af75da
AmilcarArmmand <armmand@gmail.com>
2020-03-19 21:42:45 +0000

    Added comments and deleted quoted out code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,19 +8,19 @@
  */
 size_t print_list(const list_t *h)
 {
+/* create a variable of size_t to keep track of number of structs */
 	size_t n;
 
-	n = 0;
-
+	n = 0; /* initialize it to 0 */
+/* check that the struct exists and does not contain NULL */
 	while (h != NULL)
 	{
-		if (h->str == NULL)
+		if (h->str == NULL) /* if element h->str is NULL, print nil */
 			printf("[0] (nil)\n");
-		else
+		else  /* else print the len and str elements of structs  */
 			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		n++;
+		h = h->next; /* go to the next struct */
+		n++;  /* add count of structs by 1 */
 	}
-/*	break; */
 	return (n);
 }
