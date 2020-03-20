a5c503af3bd1282d7fca04634349f71781d7cb4e
AmilcarArmmand <armmand@gmail.com>
2020-03-20 03:09:03 +0000

    Added task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+void print_before(void) __attribute__ ((constructor));
+
+/**
+ * print_before - use printf before the main function is executed.
+ */
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
