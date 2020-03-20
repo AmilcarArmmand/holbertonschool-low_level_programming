d620a1744accc4390d871c4ed083ded67525942e
AmilcarArmmand <armmand@gmail.com>
2020-03-20 21:01:59 +0000

    Updated draft of task 3 and task 6

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,5 +9,30 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
+	int length; /* counter for length of char pointer *str */
+	list_t *newNode;
+/* create and allocade memory for new node */
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+		return (NULL);
+/* put data in the node */
+	newNode->str = strdup(str);
+	if (newNode->str == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+/* get length of newNode-str */
+	for (length = 0; *(newNode->str + length); length++)
+		;
+	newNode->len = length;
+if
 
+
+/* make the next of new node as head */
+	(newNode->next) = (*head);
+/* move the head to point to the new node */
+	*head = newNode;
+
+	return (*head);
 }
