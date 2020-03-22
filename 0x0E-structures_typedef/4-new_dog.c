2520f14eb7091dcba84483f6e22c50d4eb68681f
AmilcarArmmand <armmand@gmail.com>
2020-03-22 23:10:37 +0000

    Moved check for elements of struct being NULL to separate code block

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 
 	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL || name == NULL || owner == NULL)
+	if (new_dog == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	if (name == NULL || owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog->owner);
