b6c9d9de51e8c896844bf2ca7e60f8fac45c6d09
AmilcarArmmand <armmand@gmail.com>
2020-03-24 18:31:49 +0000

    Changed head to *head in line 24

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,24 +14,25 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 /* allocate memory for the node */
 	new = malloc(sizeof(listint_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
 /* add data */
 	new->n = n;
 /* node is going to be last so set the next of new node to NULL */
 	new->next = NULL;
 /* if list is empty make new the head */
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
 	}
 	last = *head;
-	while (last->next != NULL)  /* traverse to last node of list */
+	while (last->next)  /* traverse to last node of list */
 	{
 		last = last->next;
- /* change the next element of last node */
-		last->next = new;
 	}
+ /* change the next element of last node */
+	last->next = new;
+
 	return (new);
 }
