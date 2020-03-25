45ebbaeb8e18525d5b38748dc6ad61998ff79f50
AmilcarArmmand <armmand@gmail.com>
2020-03-25 03:16:57 +0000

    Return NULL is head or *head is NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp;
 	unsigned int i;
 
-
+	if (!head || !*head)
+		return (NULL);
 
 	temp = *head;
 	for (i = 0; i < idx - 1; i++)
