67318f4dcc52c65c60761c7231c1c22215b2e1cb
AmilcarArmmand <armmand@gmail.com>
2020-03-27 01:20:56 +0000

    Added draft of task 1 code to handle 0 and 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_binary - a function that prints the binary representation of a number
+ * @n: unsigned long int to convert
+ *
+ * Return: the binary representation of n
+ */
+void print_binary(unsigned long int n)
+{
+	while (n == 0 || n == 1)
+	{
+		_putchar((n & 1) ? '1' : '0');
+		return;
+	}
+}
