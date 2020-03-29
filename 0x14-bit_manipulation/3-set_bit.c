a07cb78de6ab2fb98672aee501571c1b7db037f5
AmilcarArmmand <armmand@gmail.com>
2020-03-29 00:17:10 +0000

    Removed newNumber variable and referenced the memory adress of n, changed return to 1 if success

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,18 +2,17 @@
 
 /**
  * set_bit - a function that sets the value of a bit to 1 at given index.
- * @n: integer value
+ * @n: pointer to an integer value
  * @index: index, starting from 0, of the bit to set to 1
  *
  * Return: 1 (SUCCESS) or -1 (FAILURE)
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int newNumber;
-
+/* check that the index is less than the size of data type used */
 	if (index > sizeof(n) * 7)
 		return (-1);
-	newNumber = *n;
-	newNumber |= 1 << index;
-	return (newNumber);
+/* shift the bit at index to 1 */
+	*n = *n | 1 << index;
+	return (1);
 }
