06d3a089275eba8c2196f808aaa4c45ad5aebe36
AmilcarArmmand <armmand@gmail.com>
2020-03-31 23:59:58 +0000

    Removed + 1 from line 28

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 /* get the size of text_content with _strlen */
-	writ = write(fd, text_content, _strlen(text_content) + 1);
+	writ = write(fd, text_content, _strlen(text_content));
 
 /* if write fails, return -1 */
 	if (!writ)
