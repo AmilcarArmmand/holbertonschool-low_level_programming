829b30fa6be84171acb57f515f8dbe8727a9ded6
AmilcarArmmand <armmand@gmail.com>
2020-04-02 03:27:38 +0000

    Removed _strlen helper function and moved read to while loop

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,50 +10,43 @@
  */
 int main(int ac, char *av[])
 {
-	int file_from, file_to;   /* file descriptors */
-	ssize_t rd, writ;
-	char *buffer[1024];
+	int fd_from, fd_to;   /* file descriptors */
+	ssize_t read_from, write_to;
+	char *buffer[BUFSIZ];
 
-	if (ac != 3)
+	if (ac != 3) /* check for number of arguments */
 	{
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
-		exit(98);
+		exit(97);
 	}
-	file_from = open(av[1], O_RDONLY);
-	if (file_from  == -1)
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from  == -1)  /* open fd_from and check for success */
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	file_to = open(av[2], O_TRUNC | O_WRONLY | O_CREAT, 0644);
-	if (file_to == -1)
-	{
-		close(file_from);
+	fd_to = open(av[2], O_TRUNC | O_WRONLY | O_CREAT, 0644);
+	if (fd_to == -1)  /* open or create fd_to and check for success  */
+	{  /* check that file_from exists with right permissions  */
+		close(fd_from);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(98);
+		exit(99);
 	}
-	rd = read(file_from, buffer, 1024);
-	writ = write(file_to, buffer, rd);
-	while (rd > 0)
-		if (writ != rd)
+	read_from = read(fd_from, buffer, BUFSIZ);
+ /* loop reads to buffer & writes from buffer */
+	while ((read_from = read(fd_from, buffer, BUFSIZ)) > 0)
+	{
+		write_to = write(fd_to, buffer, read_from);
+		if (write_to != read_from) /* check if copy succeeded */
 		{
+			close(fd_from);
+			close(fd_to);
 			exit(99);
 		}
-	close(file_from);
-	close(file_to);
+	}
+	close(fd_from); /* close file_from and file_to and check for success  */
+	close(fd_to);
+	if ((close(fd_from) == -1) || (close(fd_to) == -1))
+		exit(100);
 	return (0);
-}
-
-/**
- * _strlen - a function that returns the length of a string
- * @s: pointer to string s
- * Return: length of the string
- */
-int _strlen(char *s)
-{
-	int n;
-
-	for (n = 0; s[n] != '\0'; n++)
-		;
-	return (n);
 }
