50b81ad2576af7fcbe4f262f583b61675bc8c84d
AmilcarArmmand <armmand@gmail.com>
2020-05-02 21:41:04 +0000

    Initial commit of task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - function that returns the number of elements in a linked list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of elements in the dlistint_t list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes;
+
+	nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
