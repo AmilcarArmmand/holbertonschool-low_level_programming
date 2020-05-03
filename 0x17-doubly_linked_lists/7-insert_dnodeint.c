7907b0e6d0a66a654b9981467b1494d14259f804
AmilcarArmmand <armmand@gmail.com>
2020-05-03 01:01:00 +0000

    Update task 7 to use helper functions

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,29 +14,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new;
 	dlistint_t *temp;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
 	temp = *h;
+
 	if (idx == 0)
-	{
-		new->next = temp;
-		*h = new;
-		new->prev = NULL;
-	}
+		return (add_dnodeint(h, n));
 
 	while (temp != NULL && idx > 0)
 	{
 		idx--;
-		if (idx - 1 == 0)
-		{
-			new->next = temp->next;
-			temp->next = new;
-			new->prev = temp;
-		}
 		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
 	}
+
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = temp->next;
+	new->prev = temp;
+	temp->next = new;
 
 	return (new);
 }
