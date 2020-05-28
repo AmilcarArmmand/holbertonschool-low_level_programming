b6f8799dcc99b58099d7aa979583d0ffd524de29
Amilcar Armmand <armmand@gmail.com>
2020-05-28 22:37:26 +0000

    Added task 0 and 1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - a function that creates a hash table
+ * @size: the size of the array
+ *
+ * Return: a pointer to the created hash table or NULL (FAILURE)
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i; /* index of nodes */
+
+	/* create and allocate memory for hash table */
+	ht = malloc(sizeof(hash_table_t) * size);
+	if (ht == NULL)
+		return (NULL);
+
+	/* create and initialize the node in the table with NULL values */
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t) * size);
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		(ht->array)[i] = NULL;
+	}
+
+	return (ht);
+}
