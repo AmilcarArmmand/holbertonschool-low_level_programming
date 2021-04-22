41d3b7fd6a9eb55cb46f805c9ac9df8212364e9c
AmilcarArmmand <armmand@gmail.com>
2021-04-22 04:00:32 +0000

    added draft

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,19 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - searches sorted list of integers using Jump search algorithm
+ *
+ * @list: a pointer to the head of the list to search in
+ * @size: number of nodes in the list
+ * @value: the value to search for in the array
+ *
+ * Return: first index where value is located, or NULL
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	if (list && size > 0)
+	{
+		printf("%d\n", value);
+	}
+	return (NULL);
+}
