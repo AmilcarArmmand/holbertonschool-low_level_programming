d61ec6a995e2a057a787b56089b23955c3a40797
AmilcarArmmand <armmand@gmail.com>
2021-04-23 03:46:32 +0000

    Fixed seg fault

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -13,38 +13,38 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 {
 
 	size_t step;
-	listint_t *prev;
-	listint_t *curr;
+	listint_t *prev, *curr;
 	unsigned int i;
 
-	if (list == NULL)
-		return (NULL);
-
-	/* block size to be jumped */
-	step = sqrt((int)size);
-	prev = NULL;
-	curr = list;
-
-	while (curr->next && curr->n < value)
+	if (list && size > 0)
 	{
-		printf("Value checked array[%lu] = [%d]\n", curr->index, curr->n);
-		prev = curr;
-		for (i = 0; curr->next && i < step; i++)
+		/* block size to be jumped */
+		step = sqrt(size);
+		prev = list;
+		curr = list;
+		while (curr->next && curr->index < step)
 			curr = curr->next;
-		step += sqrt(size);
-	}
-	printf("Value found between indexes ");
-	printf("[%lu] and [%lu]\n", prev->index, curr->index);
-	i = prev->index - (int)sqrt(size);
-	while (i < size && i <= prev->index)
-	{
-		printf("Value checked array[%d] = [%d]\n", i, prev->n);
-		/* if element is found */
+		printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
+		while (curr->next && curr->n < value)
+		{
+			prev = curr;
+			for (i = 0; curr->next && i < step; i++)
+				curr = curr->next;
+			printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
+		}
+		printf("Value found between indexes ");
+		printf("[%lu] and [%lu]\n", prev->index, curr->index);
+		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+		while (prev->index < (size - 1) && prev->n < value)
+		{
+
+			prev = prev->next;
+			if (prev == NULL)
+				break;
+			printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+		}
 		if (prev->n == value)
 			return (prev);
-		i++;
 	}
-
 	return (NULL);
-
 }
