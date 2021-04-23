d2f7e77dd0ef2ccd5162e8a63fdf10915332b667
AmilcarArmmand <armmand@gmail.com>
2021-04-23 02:36:49 +0000

    converting to linked list and pointer notation

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -11,9 +11,40 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	if (list && size > 0)
+
+	size_t step;
+	listint_t *prev;
+	listint_t *curr;
+	unsigned int i;
+
+	if (list == NULL)
+		return (NULL);
+
+	/* block size to be jumped */
+	step = sqrt((int)size);
+	prev = NULL;
+	curr = list;
+
+	while (curr->next && curr->n < value)
 	{
-		printf("%d\n", value);
+		printf("Value checked array[%lu] = [%d]\n", curr->index, curr->n);
+		prev = curr;
+		for (i = 0; curr->next && i < step; i++)
+			curr = curr->next;
+		step += sqrt(size);
 	}
+	printf("Value found between indexes ");
+	printf("[%lu] and [%lu]\n", prev->index, curr->index);
+	i = prev->index - (int)sqrt(size);
+	while (i < size && i <= prev->index)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, prev->n);
+		/* if element is found */
+		if (prev->n == value)
+			return (prev);
+		i++;
+	}
+
 	return (NULL);
+
 }
